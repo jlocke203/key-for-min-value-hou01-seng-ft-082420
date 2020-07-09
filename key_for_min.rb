1592567558c2491c74cc043213f07940ce06378c
Jaden Locke <jadenlocke1113@gmail.com>
2020-07-09 19:43:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,6 @@
 
 def key_for_min_value(name_hash)
 name_hash.each do |element|
-  name_hash.any? { |element| element << name_hash }  
+  name_hash.each { |element| element << name_hash }  
 end
 end 
