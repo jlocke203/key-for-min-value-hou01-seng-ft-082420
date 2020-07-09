3c0ce6d19539e9824af1541ddc6417d88953aaa6
Jaden Locke <jadenlocke1113@gmail.com>
2020-07-09 20:39:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.each do |element|
-  name_hash.each { |element| element << name_hash }  
+  men_key = nil
+  men_value = nil
+name_hash.each do |key, value|
+  if men_value == nil or value < men_value
+    men_value = value
+    men_key = key
+  end 
 end
+men_key
 end 
